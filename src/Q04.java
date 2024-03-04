6f10e22f414f2d255e1d40bddc94789390f81092
Veli Kara <113050906+velikkom@users.noreply.github.com>
2024-03-04 16:49:45 +0300

    4th question ı solved

--- src/Q04.java
+++ src/Q04.java
@@ -1,9 +1,32 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class Q04 {
 
-    /**
-     * 4)  Java Program to Find Duplicate Words in a String
-     * *
-     * Verilen bir string cümle içinde tekrar eden kelimeleri ve bu kelimelerin sayısını veren kod yazınız
+    /*
+      4)  Java Program to Find Duplicate Words in a String
+      Verilen bir string cümle içinde tekrar eden kelimeleri ve bu kelimelerin sayısını veren kod yazınız
      */
+    public static void main(String[] args) {
+        String words = "ali veli hasan hüseyin veli";
+
+        findDuplicateWords(words);
+    }
+
+    private static void findDuplicateWords(String words) {
+
+        //kelimeleri boşlukllardan böl  string arrayine at
+        String []newWords= words.split(" ");
+
+        //bir map içinde key value olarak yazdır
+        HashMap<String, Integer> myMap = new HashMap<>();
+        for (String w : newWords) {
+            myMap.put(w,myMap.getOrDefault(w,0)+1);
+        }
+
+        System.out.println(myMap);
+
+    }
+
 
 }
--- src/Q05.java
+++ src/Q05.java
@@ -1,9 +1,11 @@
 public class Q05 {
 
-    /**
-     * 5) Java Program to Find All the Permutations of a String
-    * Girilen String bir ifadenin harfleri yer değiştirilerek oluşturulabilecek anlamlı anlamsız bütün kelimeleri alt alta gösteren bir program yazınız
-    */
+    /*
+      5) Java Program to Find All the Permutations of a String
+      Girilen String bir ifadenin harfleri yer değiştirilerek oluşturulabilecek anlamlı anlamsız bütün kelimeleri alt alta gösteren bir program yazınız
+     */
+
+
 
 
 }
